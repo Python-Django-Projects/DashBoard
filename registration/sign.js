af839fd54a9588f5bb8ce42a25608242e323c32e
Ali <sanad@Alis-iMac.local>
2022-06-03 21:30:13 +0300

    register

---
+++ registration/sign.js
@@ -0,0 +1,16 @@
+const container = document.querySelector(".container"),
+    pwShowHide = document.querySelectorAll(".showHidePw"),
+    pwFields = document.querySelectorAll(".password");
+
+// JS code to show/hide password and change icon
+pwShowHide.forEach(eyeIcon => {
+    eyeIcon.addEventListener("click", () => {
+        pwFields.forEach(pwFields => {
+            if (pwFields.type === "password") {
+                pwFields.type = "text";
+            } else {
+                pwFields.type = "password"
+            }
+        })
+    })
+})
