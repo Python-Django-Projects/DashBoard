07c453f4645e2e093aedd772ed01e842b0e3ab27
User <user@Users-MacBook-Pro.local>
2022-06-13 12:46:55 +0300

    add

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ let sidebar = document.querySelector(".sidebar");
 icon.onclick = function () {
   sidebar.classList.toggle("open");
 };
+
+let myNav = document.querySelector(".fa-moon");
+myNav.onclick = function () {
+  document.body.classList.toggle("change");
+};
