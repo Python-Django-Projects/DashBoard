46870181d7ed5816fde1e318ae06b8e50108f1bd
User <user@Users-MacBook-Pro.local>
2022-06-14 14:51:22 +0300

    add

--- script.js
+++ script.js
@@ -10,31 +10,31 @@ myNav.onclick = function () {
   document.body.classList.toggle("change");
 };
 
-const toggleBtn = document.getElementById("toggle-btn");
-const theme = document.getElementById("theme");
-let darkMode = localStorage.getItem("dark-mode");
+// const toggleBtn = document.getElementById("toggle-btn");
+// const theme = document.getElementById("theme");
+// let darkMode = localStorage.getItem("dark-mode");
 
-const enableDarkMode = () => {
-  theme.classList.add("dark-mode-theme");
-  toggleBtn.classList.remove("dark-mode-toggle");
-  localStorage.setItem("dark-mode", "enabled");
-};
+// const enableDarkMode = () => {
+//   theme.classList.add("dark-mode-theme");
+//   toggleBtn.classList.remove("dark-mode-toggle");
+//   localStorage.setItem("dark-mode", "enabled");
+// };
 
-const disableDarkMode = () => {
-  theme.classList.remove("dark-mode-theme");
-  toggleBtn.classList.add("dark-mode-toggle");
-  localStorage.setItem("dark-mode", "disabled");
-};
+// const disableDarkMode = () => {
+//   theme.classList.remove("dark-mode-theme");
+//   toggleBtn.classList.add("dark-mode-toggle");
+//   localStorage.setItem("dark-mode", "disabled");
+// };
 
-if (darkMode === "enabled") {
-  enableDarkMode(); // set state of darkMode on page load
-}
+// if (darkMode === "enabled") {
+//   enableDarkMode(); // set state of darkMode on page load
+// }
 
-toggleBtn.addEventListener("click", (e) => {
-  darkMode = localStorage.getItem("dark-mode"); // update darkMode when clicked
-  if (darkMode === "disabled") {
-    enableDarkMode();
-  } else {
-    disableDarkMode();
-  }
-});
+// toggleBtn.addEventListener("click", (e) => {
+//   darkMode = localStorage.getItem("dark-mode"); // update darkMode when clicked
+//   if (darkMode === "disabled") {
+//     enableDarkMode();
+//   } else {
+//     disableDarkMode();
+//   }
+// });
